14b93201a5a692b7c8c0040940da4fd0e0e9f071
“Marcus <mcolelittle@gmail.com>
2022-08-15 12:05:58 -0400

    ssr 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,13 +3,18 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const NotFoundPage = () => (
+const NotFoundPage = ({serverData}) => (
   <Layout>
+    <Seo title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
 
-export const Head = () => <Seo title="404: Not Found" />
-
 export default NotFoundPage
+
+export async function getServerData() {
+  return {
+    status: 404,
+  }
+}
